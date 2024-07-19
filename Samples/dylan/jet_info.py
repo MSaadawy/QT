38fc71a15c86dbac44d798c533b115bee1488b25
msaadawy <mohamed.amr.essam@gmail.com>
2024-07-19 08:12:40 -0500

    jet_info.py to check on constituent link validity

--- Samples/dylan/jet_info.py
+++ Samples/dylan/jet_info.py
@@ -10,6 +10,7 @@ output_tree = ROOT.TTree("Jet_Tree", "Jet information tree")
 
 # Define variables to store jet information
 jet_pt            = ROOT.vector('float')()
+jet_E             = ROOT.vector('float')()
 jet_eta           = ROOT.vector('float')()
 jet_phi           = ROOT.vector('float')()
 jet_mass          = ROOT.vector('float')()
@@ -17,6 +18,7 @@ jet_nConstituents = ROOT.vector('int')()
 
 # Create branches in the output TTree
 output_tree.Branch("jet_pt", jet_pt)
+output_tree.Branch("jet_E", jet_E)
 output_tree.Branch("jet_eta", jet_eta)
 output_tree.Branch("jet_phi", jet_phi)
 output_tree.Branch("jet_mass", jet_mass)
@@ -25,6 +27,8 @@ output_tree.Branch("jet_nConstituents", jet_nConstituents)
 # Open the DAOD file and get the TTree
 file = ROOT.TFile.Open("DAOD_JETM42.test.pool.root", "READONLY")
 tree = ROOT.xAOD.MakeTransientTree(file, "CollectionTree")
+
+print("# of Events: ", tree.GetEntries())
 
 # Loop over entries in the TTree
 for entry in range(tree.GetEntries()):
@@ -36,16 +40,17 @@ for entry in range(tree.GetEntries()):
         # print("jet: ", jet)
         # Extract relevant information
         jet_pt.push_back(jet.pt())
+        jet_E.push_back(jet.e())
         jet_eta.push_back(jet.eta())
         jet_phi.push_back(jet.phi())
         jet_mass.push_back(jet.m())
         jet_nConstituents.push_back(jet.numConstituents ())
         # print("N-Constituents: ", jet_nConstituents)
 
-        # constituents = jet.getConstituents()
+        constituents = jet.getConstituents()
         # print("type: ", type(constituents))
         #Check the validity of the constituent links
-        # print(constituents.isValid()) #prints out True or False once
+        print(constituents.isValid()) #prints out True or False once
 
         # Uncomment if isValid() = True
         # clusters = constituents.asIParticleVector()
@@ -54,8 +59,8 @@ for entry in range(tree.GetEntries()):
         # print("Constituent Links: ", jet.constituentLinks ())
 
         # Check the validity for each constituent link individually 
-        # for link in jet.constituentLinks():
-        #     print("Validity: ", link.isValid()) # prints True or False for each link
+        for link in jet.constituentLinks():
+            print("Validity: ", link.isValid()) # prints True or False for each link
             
         #     print("link object: ", constituent)
         #     print("type of link object: ", type(constituent))
@@ -63,20 +68,21 @@ for entry in range(tree.GetEntries()):
             # clus_eta.push_back(constituent.eta())
             # clus_phi.push_back(constituent.phi())
 
-        # break   # Break out of the Jet Loop
+        break   # Break out of the Jet Loop
 
     
     # Fill the output tree
     output_tree.Fill()
 
     jet_pt.clear()
+    jet_E.clear()
     jet_eta.clear()
     jet_phi.clear()
     jet_mass.clear()
     jet_nConstituents.clear()
 
 
-    # break   # Break out of the entries loop
+    break   # Break out of the entries loop
 
 
 # Write the output file and close it
